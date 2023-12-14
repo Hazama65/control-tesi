998280a38d6e5812b734da8832f1c74a945b34a1
extcortegac_belcorp <extcortegac_belcorp@belcorp.biz>
2023-12-14 03:49:24 -0600

    cambios completos

--- src/config/controlApi.ts
+++ src/config/controlApi.ts
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const controlApi = axios.create({
-    // baseURL: 'https://back-control-production.up.railway.app/api/v1/control'
-    baseURL: 'http://localhost:3000/api/v1/control'
+    baseURL: 'https://back-control-production.up.railway.app/api/v1/control'
+    // baseURL: 'http://localhost:3000/api/v1/control'
 });
 
 controlApi.interceptors.request.use( config => {
